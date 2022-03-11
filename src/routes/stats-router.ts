979e83682435edf2b84f4a10c49fe73fe447839f
pavel <pijpinki>
2022-03-11 20:42:06 +0200

    Added curd

--- src/routes/stats-router.ts
+++ src/routes/stats-router.ts
@@ -1,4 +1,4 @@
-import { Get, Post, Body, Controller } from '@nestjs/common';
+import { Get, Post, Put, Delete, Body, Param, Controller } from '@nestjs/common';
 import { StatsModal } from '../database';
 import { AuthService } from '../services';
 
@@ -30,18 +30,18 @@ export class StatsRouter {
       type: String(object.type),
       count: parseInt(object.count, 10),
       date: String(object.date),
-    }
+    };
   }
 
   @Get()
   async getStats(): Promise<Array<StatsResponseDto>> {
     const stats = await StatsModal.findAll();
 
-    return stats.map(this.normalizeStats)
+    return stats.map(this.normalizeStats);
   }
 
   @Post()
-  async addStats(@Body() body: AddStatsDto) : Promise<Array<StatsResponseDto>> {
+  async addStats(@Body() body: AddStatsDto): Promise<Array<StatsResponseDto>> {
     await this.authService.auth(body);
 
     await StatsModal.create({
@@ -49,8 +49,32 @@ export class StatsRouter {
       type: body.type,
       count: body.count,
       date: new Date(body.date),
-    })
+    });
 
-    return (await StatsModal.findAll()).map(this.normalizeStats)
+    return (await StatsModal.findAll()).map(this.normalizeStats);
+  }
+
+  @Put(':id')
+  async editStats(@Param('id') id: number, @Body() body: AddStatsDto): Promise<Array<StatsResponseDto>> {
+    await this.authService.auth(body);
+
+    const stat = await StatsModal.findOne({ where: { id } });
+
+    if (!stat) {
+      throw new Error('Not found');
+    }
+
+    await StatsModal.update(body, { where: { id } });
+
+    return (await StatsModal.findAll()).map(this.normalizeStats);
+  }
+
+  @Delete(':id')
+  async deleteStats(@Param('id') id: number, @Body() body): Promise<Array<StatsResponseDto>> {
+    await this.authService.auth(body);
+
+    await StatsModal.destroy({ where: { id }});
+
+    return (await StatsModal.findAll()).map(this.normalizeStats);
   }
 }
