7db2c3d88e5a0301f0b9dd2a96f3ad7a1abce694
pavel <pijpinki>
2022-03-11 20:36:09 +0200

    Add add stats

--- src/routes/stats-router.ts
+++ src/routes/stats-router.ts
@@ -1,5 +1,6 @@
-import { Get, Controller } from '@nestjs/common';
+import { Get, Post, Body, Controller } from '@nestjs/common';
 import { StatsModal } from '../database';
+import { AuthService } from '../services';
 
 class StatsResponseDto {
   id: number;
@@ -8,17 +9,48 @@ class StatsResponseDto {
   date: string;
 }
 
+class AddStatsDto {
+  type: string;
+  name: string;
+  count: number;
+  date: number;
+  key: string;
+}
+
 @Controller('/stats')
 export class StatsRouter {
+  constructor(
+    private authService: AuthService
+  ) {
+  }
+
+  normalizeStats(object: any): StatsResponseDto {
+    return {
+      id: parseInt(object.id, 10),
+      type: String(object.type),
+      count: parseInt(object.count, 10),
+      date: String(object.date),
+    }
+  }
+
   @Get()
   async getStats(): Promise<Array<StatsResponseDto>> {
     const stats = await StatsModal.findAll();
 
-    return stats.map((stat: any) => ({
-      id: parseInt(stat.id, 10),
-      type: String(stat.type),
-      count: parseInt(stat.count, 10),
-      date: String(stat.date),
-    }))
+    return stats.map(this.normalizeStats)
+  }
+
+  @Post()
+  async addStats(@Body() body: AddStatsDto) : Promise<Array<StatsResponseDto>> {
+    await this.authService.auth(body);
+
+    await StatsModal.create({
+      name: body.name,
+      type: body.type,
+      count: body.count,
+      date: new Date(body.date),
+    })
+
+    return (await StatsModal.findAll()).map(this.normalizeStats)
   }
 }
